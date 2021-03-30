13f7fe7664031c39f017e78e5bae031a4ef9e2ca
Joshua Powers <powersj@fastmail.com>
2021-03-29 18:40:04 -0700

    release 21.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ PKGS, LINKS = gather_deps()
 
 setup(
     name="ubuntu-cloud-image",
-    version="20.2",
+    version="21.1",
     description="Download the newest Ubuntu cloud images.",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
